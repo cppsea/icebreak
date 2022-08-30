06b3490849f8a41d9622f708b5f4b33d4f61a427
Deric Kwok <30312964+exelarios@users.noreply.github.com>
2022-08-30 03:28:45 -0700

    init monorepo

---
+++ packages/app/.prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  useTabs: false
+};
---
+++ packages/app/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <Text>hello sea</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
