402b97cbeec9f45bf784d3d0d8be6b6b4528df9c
BradJKim <bradjkim03@gmail.com>
2022-11-02 17:37:30 -0700

    Added User Schema, Register, and Login

--- packages/server/controllers/users.js
+++ packages/server/controllers/users.js
@@ -1,5 +1,13 @@
 const postgres = require("../utils/postgres");
 
+const userSchema = new postgres.Schema({ // user details when signing up for the first time
+  first_name: { type: String, default: null },
+  last_name: { type: String, default: null },
+  email: { type: String, unique: true },
+  password: { type: String },
+  token: { type: String },
+});
+
 async function getAllUsers() {
   const query = await postgres.query('SELECT * FROM users');
   return query.rows;
--- packages/server/index.js
+++ packages/server/index.js
@@ -32,3 +32,92 @@ app.use('/api/users', users);
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 })
+
+// Register
+app.post("/register", async (req, res) => {
+
+  // Our register logic starts here
+  try {
+    // Get user input
+    const { first_name, last_name, email, password } = req.body;
+
+    // Validate user input
+    if (!(email && password && first_name && last_name)) {
+      res.status(400).send("All input is required");
+    }
+
+    // check if user already exist
+    // Validate if user exist in our database
+    const oldUser = await User.findOne({ email });
+
+    if (oldUser) {
+      return res.status(409).send("User Already Exist. Please Login");
+    }
+
+    //Encrypt user password
+    encryptedPassword = await bcrypt.hash(password, 10);
+
+    // Create user in our database
+    const user = await User.create({
+      first_name,
+      last_name,
+      email: email.toLowerCase(), // sanitize: convert email to lowercase
+      password: encryptedPassword,
+    });
+
+    // Create token
+    const token = jwt.sign(
+      { user_id: user._id, email },
+      process.env.TOKEN_SECRET, //Token secret here
+      {
+        expiresIn: "2h",
+      }
+    );
+    // save user token
+    user.token = token;
+
+    // return new user
+    res.status(201).json(user);
+  } catch (err) {
+    console.log(err);
+  }
+  // Our register logic ends here
+});
+  
+// Login
+app.post("/login", async (req, res) => {
+
+  // Our login logic starts here
+  try {
+    // Get user input
+    const { email, password } = req.body;
+
+    // Validate user input
+    if (!(email && password)) {
+      res.status(400).send("All input is required");
+    }
+    // Validate if user exist in our database
+    const user = await User.findOne({ email });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      // Create token
+      const token = jwt.sign(
+        { user_id: user._id, email },
+        process.env.TOKEN_SECRET,
+        {
+          expiresIn: "2h",
+        }
+      );
+
+      // save user token
+      user.token = token;
+
+      // user
+      res.status(200).json(user);
+    }
+    res.status(400).send("Invalid Credentials");
+  } catch (err) {
+    console.log(err);
+  }
+  // Our register logic ends here
+});
