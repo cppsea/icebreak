bd0ee5743a1297a80ba9d1af5f0c3eba6ef0e28d
E-A-H62 <85127520+E-A-H62@users.noreply.github.com>
2022-11-24 15:31:30 -0800

    feat(events): populating events on the feed  (#7)
    * Implemented register route
    
    * Working on login (must change sha256 algorithm because decrypting password is not possible)
    
    * Created refresh handling and populated event card
    
    * Cleaned up code
    
    * chore: remove backend code irrelevent to this PR
    
    * chore: remove timeout and refacor
    
    Co-authored-by: BradJKim <bradjkim03@gmail.com>
    Co-authored-by: John Dang <johndangca@gmail.com>
    Co-authored-by: Deric Kwok <30312964+exelarios@users.noreply.github.com>

--- packages/app/utils/constants.js
+++ packages/app/utils/constants.js
@@ -1,3 +1,3 @@
-const URL = "https://840b-2600-1700-7860-56a0-1c49-4af5-1a50-8afb.ngrok.io";
+const URL = "https://fc85-2620-df-8000-4707-0-2-1ab7-9e63.ngrok.io";
 
 export const ENDPOINT = `${URL}/api`;
