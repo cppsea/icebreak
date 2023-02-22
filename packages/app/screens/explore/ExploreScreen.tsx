dd83a8f83bfbfd1a6fa42773f3a1b28d1ea81626
GuyWhoCode <thatguywhocodes@gmail.com>
2023-02-21 16:26:53 -0800

    Added absolute pathing compiler option

--- packages/app/screens/explore/ExploreScreen.tsx
+++ packages/app/screens/explore/ExploreScreen.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Text } from 'react-native';
 
-import Screen from '../../components/Screen';
+import Screen from '@app/components/Screen';
 
 function ExploreScreen() {
   return (
