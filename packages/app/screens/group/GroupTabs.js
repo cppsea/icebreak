071b44d9d8d2580f7a9d4de7574a620d84347c3a
nipplelion <piolotorrico@yahoo.com>
2023-03-17 16:15:19 -0700

    Add static blue view to switch to when animation is complete

--- packages/app/screens/group/GroupTabs.js
+++ packages/app/screens/group/GroupTabs.js
@@ -21,23 +21,13 @@ const tabs = [
      screen: NewsletterScreen},
 ]
 
-const tabGroupMargin = 5;
-const tabPadding = 10;
 const blueViewWidth = 60;
-const blueViewPosition = new Animated.ValueXY({ x: tabGroupMargin + tabPadding / 2, y: 0 });
+const blueViewPosition = new Animated.ValueXY({ x: 0, y: -1 });
 
 function GroupTabs() {
     const [activeTab, setActiveTab] = useState(tabs[0]);
     const [scrollOffset, setScrollOffset] = useState(0);
-
-    useEffect(() => {
-        getPosition().then((position) => {
-            console.log("Position:" + position.toString())
-            blueViewPosition.setValue({ x: position, y: 0 });
-        }).catch((error) => {
-            console.log(error);
-        });
-    }, []);
+    const [isAnimationComplete, setIsAnimationComplete] = useState(false);
 
     // viewRefs.current to access the list
     // viewRefs.current[index].current to access the view
@@ -53,9 +43,16 @@ function GroupTabs() {
     useEffect(() => {
         getPosition().then((position) => {
             Animated.spring(blueViewPosition, {
-                toValue: { x: (position), y: blueViewPosition.y },
-                useNativeDriver: false
-                }).start();
+                toValue: { x: position, y: blueViewPosition.y },
+                useNativeDriver: false,
+                config: {
+                    speed: 1
+                }
+            }).start(() => {
+                setTimeout(() => {
+                    setIsAnimationComplete(true);
+                }, 700)
+            });
         }).catch((error) => {
             console.log(error);
         });
@@ -68,7 +65,7 @@ function GroupTabs() {
                 if (tabs[index] == activeTab && viewRefs.current[index].current) {
                     let position;
                     viewRefs.current[index].current.measure((x, y, width, height, pageX, pageY) => {
-                    position = x + width / 2 - scrollOffset - blueViewWidth / 2;
+                    position = (x + width / 2 - scrollOffset - blueViewWidth / 2) + 5;
                     resolve(position);
                     });
                     return;
@@ -78,15 +75,14 @@ function GroupTabs() {
         });
     }
 
-
     function selectTab(tab) {
         setActiveTab(tab); 
+        setIsAnimationComplete(false)
     }
 
 
     function handleScroll(event) {
         setScrollOffset(event.nativeEvent.contentOffset.x);
-        console.log("Scroll Offset: " + scrollOffset.toString())
     }
 
 
@@ -111,13 +107,16 @@ function GroupTabs() {
                                 <View style={{ alignItems: 'center' }}>
                                     <Text style={[styles.tab, {color: activeTab.name === tab.name ? '#2C2C2C' : '#717171'}]}>{tab.name}</Text>
                                 </View>
+
+                                {activeTab == tab && <View style={[styles.staticBlueView, isAnimationComplete ? {} : {opacity: 0}]} />}
                             </View>
                         </TouchableWithoutFeedback>
+                        
                     ))}
                 </View>
             </ScrollView>
 
-            <Animated.View style={[blueViewPosition.getLayout(), styles.blueView]}/>
+            <Animated.View style={[blueViewPosition.getLayout(), styles.blueView, isAnimationComplete ? {opacity: 0} : { }]}/>
             
             { activeTab.screen && <activeTab.screen /> }
         </View>
@@ -127,13 +126,13 @@ function GroupTabs() {
 const styles = StyleSheet.create({
     tab: {
         fontWeight: '600',
-        padding: tabPadding
+        padding: 10
     },
     tabGroup: {
         borderBottomColor: '#E4E4E4',
         borderBottomWidth: 3,
-        marginLeft: tabGroupMargin,
-        marginRight: tabGroupMargin
+        marginLeft: 5,
+        marginRight: 5
     },
     innerTabView: {
         flexDirection: 'row',
@@ -144,9 +143,14 @@ const styles = StyleSheet.create({
         backgroundColor: '#3498DB',
         marginTop: -2,
         borderRadius: 2
+    },
+    staticBlueView: {
+        width: blueViewWidth, 
+        height: 3, 
+        backgroundColor: '#3498DB',
+        marginBottom: -15,
+        borderRadius: 2,
     }
 });
-
-
 
 export default GroupTabs;
