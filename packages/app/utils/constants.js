c271275c8f507605bfd9b6913457b73dac65ed81
Dylan <dylanntran@protonmail.com>
2023-05-31 09:44:50 -0700

    chore: removed babel plugin line to fix type error

--- packages/app/utils/constants.js
+++ packages/app/utils/constants.js
@@ -1,3 +1,3 @@
-const URL = "https://1936-2600-1700-7860-56a0-c506-1fd3-fff9-ac77.ngrok-free.app";
+const URL = "https://e924-24-205-170-250.ngrok-free.app";
 
 export const ENDPOINT = `${URL}/api`;
