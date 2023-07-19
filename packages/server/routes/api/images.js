5fbedf564042631bbab85cb41f518ba3d9e6b879
Luke Dinh <lukexdinh@gmail.com>
2023-07-19 15:26:03 -0700

    feat: successfully created a test endpoint for retrieving images from aws s3

--- packages/server/routes/api/images.js
+++ packages/server/routes/api/images.js
@@ -1,13 +1,19 @@
 const express = require("express");
+const fs = require("fs/promises");
 const router = express.Router();
 const { s3Client } = require("../../utils/s3");
-const { PutObjectCommand } = require("@aws-sdk/client-s3");
+const { PutObjectCommand, GetObjectCommand } = require("@aws-sdk/client-s3");
+const path = require("path");
 
-router.get("/test", async (request, response) => {
+router.get("/test-insert", async (request, response) => {
+  const imageData = await fs.readFile(
+    path.resolve(__dirname + "../../../../../assets/small_banner.jpg")
+  );
+
   const command = new PutObjectCommand({
     Bucket: "icebreak-assets",
-    Key: "hello-s3.txt",
-    Body: "Hello S3!",
+    Key: "small_banner.jpg",
+    Body: imageData,
   });
 
   try {
@@ -18,6 +24,23 @@ router.get("/test", async (request, response) => {
         s3Response: s3response,
       },
     });
+  } catch (err) {
+    response.status(403).json({
+      status: "error",
+      message: err.message,
+    });
+  }
+});
+
+router.get("/test-get", async (request, response) => {
+  const command = new GetObjectCommand({
+    Bucket: "icebreak-assets",
+    Key: "small_banner.jpg",
+  });
+
+  try {
+    const s3Response = await s3Client.send(command);
+    s3Response.Body.pipe(response);
   } catch (err) {
     response.status(403).json({
       status: "error",
