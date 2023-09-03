615c23f0ac2ff74588b7f886e10596396cd262a3
Luke Dinh <lukexdinh@gmail.com>
2023-09-03 15:37:35 -0700

    chore: removed unnecessary test images api routes - fixed some formatting in api/images.js

--- packages/server/routes/api/images.js
+++ packages/server/routes/api/images.js
@@ -1,9 +1,5 @@
 const express = require("express");
-const fs = require("fs/promises");
 const router = express.Router();
-const { s3Client } = require("../../utils/s3");
-const { PutObjectCommand, GetObjectCommand, DeleteObjectCommand } = require("@aws-sdk/client-s3");
-const path = require("path");
 const { validate } = require("uuid");
 
 const AuthController = require("../../controllers/auth");
@@ -15,50 +11,6 @@ const validImageTypes = [
   "guild_banner",
   "event_banner",
 ];
-
-router.get("/test-insert", async (request, response) => {
-  const imageData = await fs.readFile(
-    path.resolve(__dirname + "../../../../../assets/small_banner.jpg")
-  );
-
-  const command = new PutObjectCommand({
-    Bucket: "icebreak-assets",
-    Key: "small_banner.jpg",
-    Body: imageData,
-  });
-
-  try {
-    const s3response = await s3Client.send(command);
-    response.json({
-      status: "success",
-      data: {
-        s3Response: s3response,
-      },
-    });
-  } catch (err) {
-    response.status(403).json({
-      status: "error",
-      message: err.message,
-    });
-  }
-});
-
-router.get("/test-get", async (request, response) => {
-  const command = new GetObjectCommand({
-    Bucket: "icebreak-assets",
-    Key: "small_banner.jpg",
-  });
-
-  try {
-    const s3Response = await s3Client.send(command);
-    s3Response.Body.pipe(response);
-  } catch (err) {
-    response.status(403).json({
-      status: "error",
-      message: err.message,
-    });
-  }
-});
 
 router.put(
   "/:type/:UUID",
@@ -96,21 +48,22 @@ router.put(
       return;
     }
 
-  try {
-    const url = await ImagesController.upload(imageType, imageData);
-    response.status(200).json({
-      status: "success",
-      data: {
-        imageURL: url,
-      },
-    });
-  } catch (err) {
-    response.status(500).json({
-      status: "error",
-      message: err.message,
-    });
+    try {
+      const url = await ImagesController.upload(imageType, imageData);
+      response.status(200).json({
+        status: "success",
+        data: {
+          imageURL: url,
+        },
+      });
+    } catch (err) {
+      response.status(500).json({
+        status: "error",
+        message: err.message,
+      });
+    }
   }
-});
+);
 
 router.get(
   "/:type/:id",
@@ -151,9 +104,10 @@ router.get(
   }
 );
 
-router.delete( "/:type/:id", 
-//AuthController.authenticate,
- async (request, response) => {
+router.delete(
+  "/:type/:id",
+  //AuthController.authenticate,
+  async (request, response) => {
     const imageType = request.params.type;
     const id = request.params.id;
     if (!validImageTypes.includes(imageType)) {
@@ -161,7 +115,7 @@ router.delete( "/:type/:id",
         status: "fail",
         data: {
           imageType: `Image type ${imageType} is invalid.`,
-        }
+        },
       });
       return;
     }
@@ -175,13 +129,12 @@ router.delete( "/:type/:id",
     //   return;
     // }
     try {
-      const deleteResponse  = await ImagesController.Delete(imageType, id);
+      const deleteResponse = await ImagesController.Delete(imageType, id);
       console.log(deleteResponse);
       response.status(200).json({
         status: "success",
-        data: null
+        data: null,
       });
-      
     } catch (err) {
       response.status(500).json({
         status: "error",
@@ -191,31 +144,32 @@ router.delete( "/:type/:id",
   }
 );
 
-router.patch( "/:type/:id", 
-//AuthController.authenticate, 
-async (request, response) => {
-  const imageType = request.params.type;
-  const id = request.params.id;
-  const imageData = request.body.imageData;
-  //  For testing
-  // const imageData = await fs.readFile(
-  //   path.resolve(__dirname + "../../../../../assets/test.jpg")
-  // );
-  if (!validImageTypes.includes(imageType)) {
-    response.status(403).json({
-      status: "fail",
-      message: `Image type ${imageType} is invalid.`,
-    });
-    return;
-  }
-  if (!imageData) {
-    response.status(400).json({
-      status: "fail",
-      message: "Request body missing values 'id' and/or 'imageData'.",
-    });
-    return;
-  }
-  // if (!validate(imageUUID)) {
+router.patch(
+  "/:type/:id",
+  //AuthController.authenticate,
+  async (request, response) => {
+    const imageType = request.params.type;
+    const id = request.params.id;
+    const imageData = request.body.imageData;
+    //  For testing
+    // const imageData = await fs.readFile(
+    //   path.resolve(__dirname + "../../../../../assets/test.jpg")
+    // );
+    if (!validImageTypes.includes(imageType)) {
+      response.status(403).json({
+        status: "fail",
+        message: `Image type ${imageType} is invalid.`,
+      });
+      return;
+    }
+    if (!imageData) {
+      response.status(400).json({
+        status: "fail",
+        message: "Request body missing values 'id' and/or 'imageData'.",
+      });
+      return;
+    }
+    // if (!validate(imageUUID)) {
     //   response.status(400).json({
     //     status: "fail",
     //     data: {
@@ -224,20 +178,21 @@ async (request, response) => {
     //   });
     //   return;
     // }
-  try {
-    const url = await ImagesController.patch(imageType, imageData, id);
-    response.status(200).json({
-      status: "success",
-      data:{
-        imageURL: url
-      }
-    });
-  } catch (err) {
-    response.status(500).json({
-      status: "error",
-      message: err.message,
-    });
+    try {
+      const url = await ImagesController.patch(imageType, imageData, id);
+      response.status(200).json({
+        status: "success",
+        data: {
+          imageURL: url,
+        },
+      });
+    } catch (err) {
+      response.status(500).json({
+        status: "error",
+        message: err.message,
+      });
+    }
   }
-});
+);
 
 module.exports = router;
