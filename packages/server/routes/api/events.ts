93022709bbbf8a25fb1bd1a07800e9e1a4fc8d41
mikephu <mikephu03@gmail.com>
2023-10-25 01:39:25 -0700

    feat: migrate events/guilds routes

--- packages/server/routes/api/events.ts
+++ packages/server/routes/api/events.ts
@@ -1,9 +1,24 @@
-const express = require("express");
+import express from "express";
 const router = express.Router();
+const DEFAULT_EVENT_LIMIT: number = 10;
 
-const EventController = require("../../controllers/events");
-const AuthController = require("../../controllers/auth");
-const DEFAULT_EVENT_LIMIT = 10;
+import EventController from "../../controllers/events";
+import AuthController from "../../controllers/auth";
+import { APIRequest, APIResponse } from "../../types/index"
+import { Event } from "@prisma/client";
+
+type APIRequestCursor = {
+  cursor: string,
+}
+
+type APIResponseCursor = {
+  events: Event[],
+  totalPages?: number,
+  cursor: {
+    previousPage: string | null,
+    nextPage: string | null,
+  }
+}
 
 /**
  * cursor is base-64 encoded and formatted as
@@ -14,9 +29,11 @@ const DEFAULT_EVENT_LIMIT = 10;
 router.get(
   "/pages/:cursor?",
   AuthController.authenticate,
-  async (request, response) => {
+  async (request: APIRequest<APIRequestCursor,void>, response: APIResponse<APIResponseCursor>) => {
     try {
-      if (request.query.limit && isNaN(request.query.limit)) {
+      let queryLimit: string = request.query.limit as string
+
+      if (queryLimit && isNaN(parseInt(queryLimit))) {
         return response.status(400).json({
           status: "fail",
           data: {
@@ -25,17 +42,22 @@ router.get(
         });
       }
 
-      const eventLimit = parseInt(request.query.limit) || DEFAULT_EVENT_LIMIT;
+      const eventLimit: number = parseInt(queryLimit) || DEFAULT_EVENT_LIMIT;
       // base64 decode cursor parameter if not null
-      const requestCursor = request.params.cursor
+      const requestCursor: string = request.params.cursor
         ? Buffer.from(request.params.cursor, "base64").toString()
         : "";
-      let [currentPage, action, eventId] = requestCursor.split("___");
-      currentPage = parseInt(currentPage) || 1;
-      const events = await EventController.getEvents(
+
+      // let [currentPage, action, eventId] = requestCursor.split("___");
+      let cursor: string[] = requestCursor.split("___");
+      let currentPage: number = parseInt(cursor[0]) || 1;
+      let action: string = cursor[1]
+      let eventId: string = cursor[2]
+
+      const events: Event[] = await EventController.getEvents(
         eventLimit,
         action,
-        eventId
+        eventId,
       );
 
       if (events.length === 0) {
@@ -45,12 +67,12 @@ router.get(
       // generate cursors for previous and next pages
       // const firstEventId = events[0].event_id;
       // const lastEventId = events[events.length - 1].event_id;
-      const firstEventId = events[0].eventId;
-      const lastEventId = events[events.length - 1].eventId;
-      const prevCursor = Buffer.from(
+      const firstEventId: string = events[0].eventId;
+      const lastEventId: string = events[events.length - 1].eventId;
+      const prevCursor: string = Buffer.from(
         `${currentPage - 1}___prev___${firstEventId}`
       ).toString("base64");
-      const nextCursor = Buffer.from(
+      const nextCursor: string = Buffer.from(
         `${currentPage + 1}___next___${lastEventId}`
       ).toString("base64");
 
@@ -69,7 +91,7 @@ router.get(
       } else {
         // first request made to this route, determine total number of pages
         // null previous cursor on first page
-        const totalPages = await EventController.getPages(eventLimit);
+        const totalPages: number = await EventController.getPages(eventLimit);
 
         response.status(200).json({
           status: "success",
@@ -84,21 +106,37 @@ router.get(
         });
       }
     } catch (error) {
-      response.status(500).json({
+      if(error instanceof Error) {
+        response.status(500).json({
         status: "error",
         message: error.message,
-      });
+        })
+      }
+      else {
+        response.status(500).json({
+        status: "error",
+        message: "An unknown error has occured",
+        });
+      }
     }
   }
 );
 
+type APIRequestGetEvent = {
+  eventId: string,
+}
+
+type APIResponseGetEvent = {
+  event: Event | null,
+}
+
 router.get(
   "/:eventId",
   AuthController.authenticate,
-  async (request, response) => {
+  async (request: APIRequest<APIRequestGetEvent,void>, response:APIResponse<APIResponseGetEvent>) => {
     try {
       const { eventId } = request.params;
-      const event = await EventController.getEvent(eventId);
+      const event: Event | null = await EventController.getEvent(eventId);
       response.status(200).json({
         status: "success",
         data: {
@@ -106,10 +144,18 @@ router.get(
         },
       });
     } catch (error) {
-      response.status(500).json({
+      if(error instanceof Error) {
+        response.status(500).json({
         status: "error",
         message: error.message,
-      });
+        })
+      }
+      else {
+        response.status(500).json({
+        status: "error",
+        message: "An unknown error has occured",
+        });
+      }
     }
   }
 );
