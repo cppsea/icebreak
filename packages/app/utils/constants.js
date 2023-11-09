357aa61d52536a9a8f8365d7f3e369d88890f5c2
Luke Dinh <lukexdinh@gmail.com>
2023-11-08 17:03:51 -0800

    feat: Implementing Icebreak media asset storage with AWS S3 (#25)
    * implemented image CRUD operations for aws s3
    
    * Error Handling for Valid Base64 JPEG
    
    * implemented nonexistent database entity fail response
    
    ---------
    
    Co-authored-by: shotoh <shotoh0806@gmail.com>
    Co-authored-by: shotoh <trueshotoh@gmail.com>
    Co-authored-by: steven chen <huanshengc@cpp.edu>

--- packages/app/utils/constants.js
+++ packages/app/utils/constants.js
@@ -1,3 +1,4 @@
-const URL = "https://9bcd-2600-1700-7860-56a0-519a-a0e9-d222-39ae.ngrok-free.app";
+const URL =
+  "https://915c-2603-8000-1101-4589-4418-4418-3d0f-e142.ngrok-free.app";
 
 export const ENDPOINT = `${URL}/api`;
--- packages/server/index.js
+++ packages/server/index.js
@@ -12,6 +12,7 @@ const users = require("./routes/api/users");
 const guilds = require("./routes/api/guilds");
 const events = require("./routes/api/events");
 const auth = require("./routes/api/auth");
+const images = require("./routes/api/images");
 
 app.use(
   cors({
@@ -21,7 +22,7 @@ app.use(
 );
 
 app.use(cookieParser());
-app.use(express.json());
+app.use(express.json({ limit: "20mb" }));
 
 app.get("/", async (request, response) => {
   response.send("Hello SEA!");
@@ -31,6 +32,7 @@ app.use("/api/auth", auth);
 app.use("/api/users", users);
 app.use("/api/guilds", guilds);
 app.use("/api/events", events);
+app.use("/api/media/images", images);
 
 const server = app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
