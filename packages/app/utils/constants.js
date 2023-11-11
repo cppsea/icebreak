931eed29bf909f2c2a4296c59a7e617bd26d9e2c
Luke Dinh <lukexdinh@gmail.com>
2023-11-11 10:25:25 -0800

    feat(ci-cd): added pre-commit and commit-msg hooks
    - pre-commit hook for linting and formatting code
    - commit-msg hook to enforce conventional commit msg format

--- packages/app/utils/constants.js
+++ packages/app/utils/constants.js
@@ -1,3 +1,4 @@
-const URL = "https://9bcd-2600-1700-7860-56a0-519a-a0e9-d222-39ae.ngrok-free.app";
+const URL =
+  "https://9bcd-2600-1700-7860-56a0-519a-a0e9-d222-39ae.ngrok-free.app";
 
 export const ENDPOINT = `${URL}/api`;
