c125e58478fe362b0c11a08c3118a869a7cb5b8e
Sam Ly <sam@samly.me>
2023-11-16 13:52:21 -0800

    fix: use findUnique

--- packages/server/controllers/guilds.js
+++ packages/server/controllers/guilds.js
@@ -1,27 +1,24 @@
 const prisma = require("../prisma/prisma");
 
 async function getAllGuilds() {
-  const query = await prisma.guilds.findMany();
-  return query;
+  return await prisma.guilds.findMany();
 }
 
 async function getGuild(guildId) {
-  const query = await prisma.guilds.findFirst({
+  return await prisma.guilds.findUnique({
     where: {
       guildId: guildId,
     },
   });
-  return query;
 }
 
 // TODO: Remove guildId param
 async function createGuild(eventData) {
   try {
-    const created_guild = await prisma.guilds.create({
+    return await prisma.guilds.create({
       // Guild Data
       data: eventData,
     });
-    return created_guild;
   } catch (error) {
     return null;
   }
@@ -29,14 +26,13 @@ async function createGuild(eventData) {
 
 async function updateGuild(guildId, eventData) {
   try {
-    const updated_guild = await prisma.guilds.update({
+    return await prisma.guilds.update({
       where: {
         guildId: guildId,
       },
       // Guild Data
       data: eventData,
     });
-    return updated_guild;
   } catch (error) {
     return null;
   }
@@ -44,12 +40,11 @@ async function updateGuild(guildId, eventData) {
 
 async function deleteGuild(guildId) {
   try {
-    const deleted_guild = await prisma.guilds.delete({
+    return await prisma.guilds.delete({
       where: {
         guildId: guildId,
       },
     });
-    return deleted_guild;
   } catch (error) {
     return null;
   }
