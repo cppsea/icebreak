86a7a11a66bd61fdf4c8440132aba05f4cd9b4fb
Sam Ly <sam@samly.me>
2023-11-19 21:56:49 -0800

    fix: controller functions now throw errors

--- packages/server/controllers/guilds.js
+++ packages/server/controllers/guilds.js
@@ -12,61 +12,27 @@ async function getGuild(guildId) {
   });
 }
 
-async function createGuild(eventData) {
-  const data = cleanEventData(eventData);
-  try {
-    return await prisma.guilds.create({
-      data: data,
-    });
-  } catch (error) {
-    return null;
-  }
+async function createGuild(guildData) {
+  return await prisma.guilds.create({
+    data: guildData,
+  });
 }
 
-async function updateGuild(guildId, eventData) {
-  const data = cleanEventData(eventData);
-  try {
-    return await prisma.guilds.update({
-      where: {
-        guildId: guildId,
-      },
-      data: data,
-    });
-  } catch (error) {
-    return null;
-  }
+async function updateGuild(guildId, guildData) {
+  return await prisma.guilds.update({
+    where: {
+      guildId: guildId,
+    },
+    data: guildData,
+  });
 }
 
 async function deleteGuild(guildId) {
-  try {
-    return await prisma.guilds.delete({
-      where: {
-        guildId: guildId,
-      },
-    });
-  } catch (error) {
-    return null;
-  }
-}
-
-// This function returns an object that only has fields
-// specified by the Prisma schema, cleans the request body.
-function cleanEventData(eventData) {
-  return {
-    name: eventData.name || "",
-    handler: eventData.handler || "",
-    description: eventData.description || "",
-    category: eventData.category || "",
-    location: eventData.location || "",
-    website: eventData.website || "",
-    tags: eventData.tags || [],
-    banner: eventData.banner || "",
-    icon: eventData.icon || [],
-    media: eventData.media || "",
-    isInviteOnly: eventData.isInviteOnly || false,
-    events: eventData.events || [],
-    members: eventData.members || [],
-  };
+  return await prisma.guilds.delete({
+    where: {
+      guildId: guildId,
+    },
+  });
 }
 
 module.exports = {
