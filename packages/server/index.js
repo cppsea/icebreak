5e86283912018d1c7710230b2b99fa9aed911a15
Luke Dinh <lukexdinh@gmail.com>
2023-11-21 13:11:49 -0800

    fix: re-added dependencies removed from merge commit

--- packages/server/index.js
+++ packages/server/index.js
@@ -22,11 +22,7 @@ app.use(
 );
 
 app.use(cookieParser());
-<<<<<<< HEAD
-app.use(express.json());
-=======
 app.use(express.json({ limit: "20mb" }));
->>>>>>> main
 
 app.get("/", async (request, response) => {
   response.send("Hello SEA!");
