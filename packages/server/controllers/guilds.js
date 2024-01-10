9ca066a5b0ca602d67c2c7a0ce8a389f4562a85c
shotoh <trueshotoh@gmail.com>
2024-01-09 19:26:52 -0800

    fix: replaced similarity search with word similarity search

--- packages/server/controllers/guilds.js
+++ packages/server/controllers/guilds.js
@@ -39,15 +39,15 @@ async function deleteGuild(guildId) {
 async function searchGuildByName(pattern) {
   return prisma.$queryRaw`
     SELECT guild_id, name, handler, icon FROM guilds 
-    WHERE SIMILARITY(name, ${pattern}) > ${MINIMUM_SIMILARITY}
-    ORDER BY SIMILARITY(name, ${pattern}) DESC;`;
+    WHERE WORD_SIMILARITY(${pattern}, name) > ${MINIMUM_SIMILARITY}
+    ORDER BY WORD_SIMILARITY(${pattern}, name) DESC;`;
 }
 
 async function searchGuildByHandler(pattern) {
   return prisma.$queryRaw`
     SELECT guild_id, name, handler, icon FROM guilds 
-    WHERE SIMILARITY(handler, ${pattern}) > ${MINIMUM_SIMILARITY}
-    ORDER BY SIMILARITY(handler, ${pattern}) DESC;`;
+    WHERE SIMILARITY(${pattern}, handler) > ${MINIMUM_SIMILARITY}
+    ORDER BY SIMILARITY(${pattern}, handler) DESC;`;
 }
 
 async function guildExists(guildId) {
