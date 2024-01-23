a742d09305f40669c7ec834ad59e263ff25e36d8
Michael <mwu250@gmail.com>
2024-01-22 18:27:27 -0800

    feat: added styling and display GroupIcon instead of RoundedIcon

--- packages/app/screens/group/GroupCard.js
+++ packages/app/screens/group/GroupCard.js
@@ -1,12 +1,26 @@
-import RoundedIcon from "@app/components/MemberCard/RoundedIcon";
+// import RoundedIcon from "@app/components/MemberCard/RoundedIcon";
+import GroupIcon from "@app/components/GroupScreen/GroupIcon";
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import PropTypes from "prop-types";
 
+import { useGuildContext } from "@app/utils/GuildContext";
+
 const GroupCard = (props) => {
+  const { guild } = useGuildContext();
+  const iconSize = 50;
+
   return (
     <View style={styles.card}>
-      <RoundedIcon></RoundedIcon>
+      {/* <RoundedIcon></RoundedIcon> */}
+      <View style={styles.avatar}>
+        <GroupIcon
+          testID="groupIcon"
+          icon={guild.avatar}
+          size={iconSize}
+          backgroundColor={"#0E131F"}
+        />
+      </View>
       <View style={styles.text}>
         <Text style={styles.name}>{props.name}</Text>
         <Text style={styles.handle}>{props.handle}</Text>
@@ -25,15 +39,31 @@ const grey = "#808080";
 const white = "#fff";
 
 const styles = StyleSheet.create({
+  // card: {
+  //   alignItems: "center",
+  //   backgroundColor: white,
+  //   borderRadius: 10,
+  //   elevation: 3,
+  //   flexDirection: "row",
+  //   margin: 10,
+  //   overflow: "hidden",
+  //   padding: 10,
+  // shadowColor: black,
+  // shadowOffset: { width: 0, height: 2 },
+  // shadowOpacity: 0.2,
+  // shadowRadius: 5,
+  // },
+  avatar: {
+    width: 60,
+  },
   card: {
     alignItems: "center",
     backgroundColor: white,
-    borderRadius: 5,
-    elevation: 3,
+    borderRadius: 10,
     flexDirection: "row",
-    marginBottom: 10,
-    overflow: "hidden",
-    padding: 10,
+    margin: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 15,
     shadowColor: black,
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.2,
--- packages/app/screens/group/GroupList.js
+++ packages/app/screens/group/GroupList.js
@@ -1,22 +1,75 @@
 import React from "react";
-import { View, FlatList } from "react-native";
+import { View, FlatList, Text, StyleSheet, Platform } from "react-native";
 import GroupCard from "./GroupCard";
+import { GuildProvider } from "@app/utils/GuildContext.js";
+import PropTypes from "prop-types";
+
+const shadow = "rgba(0, 0, 0, 0.25)";
+const titleColor = "rgb(51,51,51)";
+
+const styles = StyleSheet.create({
+  card: {
+    borderRadius: 15,
+    marginBottom: 10,
+    padding: 10,
+  },
+  container: {
+    flex: 1,
+    paddingHorizontal: 5,
+    paddingTop: 50,
+    ...Platform.select({
+      ios: {
+        shadowColor: shadow,
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.8,
+        shadowRadius: 4,
+      },
+      android: {
+        elevation: 4,
+      },
+    }),
+  },
+  title: {
+    color: titleColor,
+    fontSize: 30,
+    fontWeight: "bold",
+    margin: 15,
+    marginBottom: 10,
+  },
+});
 
 const groups = [
   { id: "1", name: "Software Engineering Association", handle: "@cppsea" },
   { id: "2", name: "Developers Guild", handle: "@devguild" },
 ];
 
-const GroupList = () => {
+function GroupList({ route }) {
   return (
-    <View>
-      <FlatList
-        data={groups}
-        renderItem={({ item }) => <GroupCard group={item} />}
-        keyExtractor={(item) => item.id}
-      />
+    <View style={styles.container}>
+      <GuildProvider guildId={route.params?.guildId}>
+        <Text style={styles.title}>Your Groups</Text>
+        <FlatList
+          data={groups}
+          renderItem={({ item }) => (
+            <GroupCard
+              style={styles.card}
+              name={
+                item.name.length <= 28
+                  ? item.name
+                  : item.name.substring(0, 25) + "..."
+              }
+              handle={item.handle}
+            />
+          )}
+          keyExtractor={(item) => item.id}
+        />
+      </GuildProvider>
     </View>
   );
+}
+
+GroupList.propTypes = {
+  route: PropTypes.any,
 };
 
 export default GroupList;
