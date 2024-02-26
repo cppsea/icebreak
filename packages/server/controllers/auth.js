55db2c19d305e0c94b356d651eb9865727750e35
Sam Ly <sam@samly.me>
2024-02-25 18:23:20 -0800

    feat: implement is google account

--- packages/server/controllers/auth.js
+++ packages/server/controllers/auth.js
@@ -202,10 +202,15 @@ async function verifyUserEmail(email) {
 }
 
 async function isGoogleAccount(userId) {
-  // TODO: implement is google account
-  if (userId) return false;
+  const result = await prisma.findUnique({
+    where: {
+      userId: userId,
+    }
+  });
 
-  // If password is null then it is a google account.
+  if (result.password.length() == 0) return true;
+
+  return false;
 }
 
 // TODO: implement send password reset email
