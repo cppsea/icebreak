3a7d200e9d1a223e7c9db97166be1b6e08a7a31c
Larry La <larryquocla@gmail.com>
2024-03-03 23:44:01 -0800

    feat: unit tests

--- packages/server/__tests__/controllers/auth.test.js
+++ packages/server/__tests__/controllers/auth.test.js
@@ -1,2 +1,51 @@
-// TODO: Unit tests for password reset route (redismock, prismamock)
-// TODO: add unit tests for forgot password route (NOTE: ONLY AFTER WORKFLOW ARE WORKING NICELY)
+const { prismaMock } = require("../prisma_mock");
+const { isUserEmail, isGoogleAccount } = require("../../controllers/auth");
+
+describe("Forgot Password User Verification Unit Tests", () => {
+  test("should return true if user email exists", async () => {
+    const testEmail = "userthatexists@gmail.com";
+    const testUser = {
+      email: testEmail,
+    };
+    prismaMock.users.findUnique.mockResolvedValue(testUser);
+
+    const result = await isUserEmail(testEmail);
+
+    expect(result).toEqual(true);
+  });
+
+  test("should return false if user email does not exist", async () => {
+    const testEmail = "userthatisnonexistent@example.com";
+    prismaMock.users.findUnique.mockResolvedValue(null);
+
+    const result = await isUserEmail(testEmail);
+
+    expect(result).toEqual(false);
+  });
+
+  test("should return true if user is a Google account", async () => {
+    const testUserId = "googlegeneratedaccountid";
+    const testUser = {
+      userId: testUserId,
+      password: null,
+    };
+    prismaMock.users.findUnique.mockResolvedValue(testUser);
+
+    const result = await isGoogleAccount(testUserId);
+
+    expect(result).toEqual(true);
+  });
+
+  test("should return false if user is not a Google account", async () => {
+    const testUserId = "nongooglegeneratedaccountid";
+    const testUser = {
+      userId: testUserId,
+      password: "hashedPassword",
+    };
+    prismaMock.users.findUnique.mockResolvedValue(testUser);
+
+    const result = await isGoogleAccount(testUserId);
+
+    expect(result).toEqual(false);
+  });
+});
