3705f6baa65d45ffc2a5a08a7d9ee672a0e8e4ee
Michael <mwu250@gmail.com>
2024-03-18 20:48:03 -0700

    fix: moved validation for user in guild to userId body field

--- packages/server/validators/users.js
+++ packages/server/validators/users.js
@@ -1,6 +1,8 @@
 const { param, body } = require("express-validator");
 
 const UserController = require("../controllers/users");
+const EventController = require("../controllers/events");
+const GuildController = require("../controllers/guilds");
 
 const userIdValidator = [
   param("userId")
@@ -32,6 +34,17 @@ const userIdBodyValidator = [
         await UserController.getUser(value);
       } catch (error) {
         throw new Error("User with ID ${value} not found");
+      }
+    })
+    .custom(async (userId, { req }) => {
+      const eventId = req.params.eventId;
+      const event = await EventController.getEvent(eventId);
+      const isMember = await GuildController.isGuildMember(
+        event.guildId,
+        userId
+      );
+      if (!isMember) {
+        throw new Error("User is not a member of the guild.");
       }
     }),
 ];
