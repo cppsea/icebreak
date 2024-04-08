8abb0b5efb8a3d2393b2b5aaa3583c31d6141495
MinT-Napkin <mtran8011@yahoo.com>
2024-04-07 18:26:04 -0700

    chore: update expo-image-picker dependency

--- packages/app/utils/constants.js
+++ packages/app/utils/constants.js
@@ -1,4 +1,3 @@
-const URL =
-  "https://bdb9-2600-1700-7860-56a0-798e-f3fa-ddb3-f01d.ngrok-free.app";
+const URL = "https://0149-172-88-118-140.ngrok-free.app";
 
 export const ENDPOINT = `${URL}/api`;
